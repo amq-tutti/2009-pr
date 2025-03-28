e1fe7f5c3b4beaf03e9b4cd2a5ed0b7a5d1d0bcc
dvitjuks <d.vitjuks@gmail.com>
2025-03-28 18:30:25 +0200

    Change HTML title a bit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     localStoragePrefix: "2009-pr",
-    title: "2009 Anime Songs Sorter",
+    title: "2009 Anime Songs Party Rank Sorter",
     description: "Party rank sorter for nominated 2009 anime songs"
 };
